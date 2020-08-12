dd1082a5f8fade31e2c4ac7cbd03f183cc279d25
yangjaemo <69129378+yangjaemo@users.noreply.github.com>
2020-08-12 14:32:46 +0900

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost,http://smcyjm.dothome.co.kr/";
-$dbname = 'users';
-$user = 'smcyjm';
-$password = 'gosu3308*^^*';
-$pdo = new PDO(
-        'mysql:host=' . $host . ';dbname=' . $dbname, $user, $password
-);
