5f909b032f2ff33397f9545be7c3079f61a7d740
yangjaemo <69129378+yangjaemo@users.noreply.github.com>
2020-08-15 23:45:57 +0900

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,14 +1,15 @@
 <?php
-$db_host="http://smcyjm.dothome.co.kr/"; //localhost server 
+$db_host="http://smcyjm.dothome.co.kr"; //localhost server 
 $db_user="smcyjm'"; //database username
 $db_password="gosu3308*^^*"; //database password   
 $db_name="users"; //database name
 
 try
 {
- $db=new PDO("mysql:host={$servername};dbname={$database}",$username,$password);
+ $db=new PDO("mysql:host={$db_host};dbname={$db_user}",$username,$password);
  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+
 catch(PDOEXCEPTION $e)
 {
  $e->getMessage();
